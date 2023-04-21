2a858abc5d3ff73c0ede30c714257b523af262ed
Zodak <ojepaul4jesus@gmail.com>
2023-04-21 10:51:09 +0100

    outline issue #4

--- components/atoms/InputField.tsx
+++ components/atoms/InputField.tsx
@@ -23,9 +23,8 @@ export function InputField(props: IInputFieldProps) {
           <select
             onChange={(e) => props.setValue(e.target.value)}
             className={` ${
-              !props.valid &&
-              "focus:outline focus:outline-offset-2 focus:outline-[0.2rem] focus:outline-red-100"
-            } px-4 p-2 bg-bgGrey w-full h-10 mt-2 rounded-md  border-0 focus:outline focus:outline-offset-2 focus:outline-[0.2rem] focus:outline-teal-100`}
+              !props.valid && " focus:ring-red-100 "
+            } px-4 p-2 bg-bgGrey w-full h-10 mt-2 rounded-md  border-0 focus:ring-offset-2 focus:ring-[0.2rem] focus:ring-teal-100 focus:border-transparent outline-none`}
             id={props.name}
           >
             <option></option>
@@ -38,9 +37,8 @@ export function InputField(props: IInputFieldProps) {
             onChange={(e) => props.setValue(e.target.value)}
             // className="focus:border-transparent focus:ring-0"
             className={`${
-              !props.valid &&
-              "focus:ring-offset-2 focus:ring-[0.2rem] focus:ring-red-100 focus:border-transparent "
-            }  bg-bgGrey mt-2 w-full h-20 rounded-md h-40 border-0  focus:ring-offset-2 focus:ring-[0.2rem] focus:ring-teal-100 `}
+              !props.valid && " focus:ring-red-100  "
+            }  bg-bgGrey mt-2 w-full h-20 rounded-md h-40 border-0  focus:ring-offset-2 focus:ring-[0.2rem] focus:ring-teal-100 focus:border-transparent outline-none`}
             placeholder={props.placeholder}
           />
         ) : (
@@ -48,9 +46,8 @@ export function InputField(props: IInputFieldProps) {
             type={props.type}
             placeholder={props.placeholder}
             className={`${
-              !props.valid &&
-              "focus:outline focus:outline-offset-2 focus:outline-[0.2rem] focus:outline-red-100"
-            }  focus:ring-offset-2 focus:ring-[0.2rem] focus:ring-teal-100 bg-bgGrey w-full border-0 ring-none  h-full mt-2 rounded-md px-4`}
+              !props.valid && " focus:ring-red-100 "
+            }  focus:ring-offset-2 focus:ring-[0.2rem] border-0 focus:ring-teal-100 focus:border-transparent outline-none bg-bgGrey w-full border-0 ring-none  h-full mt-2 rounded-md px-4`}
             id={props.name}
             onChange={(e: any) => {
               props.setValue(e.target.value);
